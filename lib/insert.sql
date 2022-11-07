4ae8b995ee0a5896e3535b43d46f50ec89f04e95
keveeen <105822487+keveeen@users.noreply.github.com>
2022-11-07 16:08:13 +0100

    commit tree 38f7a2925939ed57fb325cd8b7fd0d959471ad59

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed (refer back to how to create a record that doesn't have one value)
+
+INSERT INTO bears (name, sex, color) VALUES ("Mr. Chocolate", "M", "Brown");
+INSERT INTO bears (name, sex, color) VALUES ("Rowdy", "M", "Auburn");
+INSERT INTO bears (name, sex, color) VALUES ("Tabitha", "F", "Brown");
+INSERT INTO bears (name, sex, color) VALUES ("Sergeant Brown", "M", "Brown");
+INSERT INTO bears (name, sex, color) VALUES ("Melissa", "F", "Black");
+INSERT INTO bears (name, sex, color) VALUES ("Grinch", "M", "Green");
+INSERT INTO bears (name, sex, color) VALUES ("Wendy", "F", "Black");
+INSERT INTO bears (name, sex, color) VALUES (NULL, "F", "Brown");
